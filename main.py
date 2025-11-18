eb2a0d8cdb57e7db59c09b39034821acca3a294b
I-like-penguins <ThomasRe@mailbox.org>
2025-11-18 19:12:15 +0100

    finished Store-object

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from products import Product
+from store import Store
 
 
 def main():
     bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
     mac = Product("MacBook Air M2", price=1450, quantity=100)
 
-    print(bose.buy(50))
-    print(mac.buy(100))
-    print(mac.is_active())
+    best_buy = Store([bose, mac])
+    price = best_buy.order([(bose, 5), (mac, 30), (bose, 10)])
+    print(f"Order cost: {price} dollars.")
 
-    bose.show()
-    mac.show()
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
 
-    bose.set_quantity(1000)
-    bose.show()
+    best_buy = Store(product_list)
+    products = best_buy.get_active_products()
+    print(f"Total Quantity:  {best_buy.get_total_quantity()}")
+    print(f"Number of active products: {best_buy.get_number_of_active_products()}")
+    print(f"Sum: {best_buy.order([(products[0], 1), (products[1], 2)])}")
 
 if __name__ == "__main__":
     main()
--- store.py
+++ store.py
@@ -17,3 +17,47 @@ class Store:
             raise TypeError("Product must be of type Product")
         self.__products.append(product)
 
+    def remove_product(self, product):
+        """Remove a product-object from the store, raise an error if the product is not of type Product"""
+        if not isinstance(product, pd.Product):
+            raise TypeError("Product must be of type Product")
+        self.__products.remove(product)
+
+    def get_products(self):
+        """Return a list of every product-object, active or not"""
+        return self.__products
+
+    def get_active_products(self) -> list[pd.Product]:
+        """Return a list of every product-object that is active"""
+        active_products = []
+        for product in self.__products:
+            if product.is_active():
+                active_products.append(product)
+        return active_products
+
+    def get_total_quantity(self) -> int:
+        """Return the total quantity of all products"""
+        total_quantity = 0
+        for product in self.__products:
+            total_quantity += product.get_quantity()
+        return total_quantity
+
+    def get_number_of_active_products(self) -> int:
+        """Return the number of active products"""
+        return len(self.get_active_products())
+
+    def order(self, shopping_list) -> float:
+        """Return the total price of the shopping list, raise an error if the shopping list is not a list of Product-objects
+        shopping_list consists of Tuples of (Product-object, quantity)"""
+        total_price = 0.0
+        for product, quantity in shopping_list:
+           if not isinstance(product, pd.Product):
+               raise TypeError("All products must be of type Product")
+           if not isinstance(quantity, int):
+               raise TypeError("Quantity must be of type int")
+           if not product.is_active():
+               raise ValueError("Product is not active")
+           if quantity <= 0:
+               raise ValueError("Quantity cannot be less or equal to zero")
+           total_price += product.buy(quantity)
+        return total_price
